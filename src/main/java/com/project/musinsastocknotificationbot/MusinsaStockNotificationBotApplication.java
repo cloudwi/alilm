150bd6203ae7fcf9ef301595824504488d207ed8
cloudwi <cloudwi@naver.com>
2023-05-31 16:26:49 +0900

    feat : 리드미 파일 수정
    - 사용된 기술 추가

--- src/main/java/com/project/musinsastocknotificationbot/MusinsaStockNotificationBotApplication.java
+++ src/main/java/com/project/musinsastocknotificationbot/MusinsaStockNotificationBotApplication.java
@@ -13,5 +13,4 @@ public class MusinsaStockNotificationBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(MusinsaStockNotificationBotApplication.class, args);
     }
-
 }
