c55b22cf0b219eed8c045788b9fdd6e3ffac2634
cloudwi <cloudwi@naver.com>
2023-06-02 09:07:16 +0900

    refactoring : TelegramService의 책임을 다른 Service레이어로 전달했습니다.

--- src/main/java/com/project/musinsastocknotificationbot/telegramBot/service/TelegramService.java
+++ src/main/java/com/project/musinsastocknotificationbot/telegramBot/service/TelegramService.java
@@ -1,18 +1,17 @@
 package com.project.musinsastocknotificationbot.telegramBot.service;
 
-import com.project.musinsastocknotificationbot.product.domain.Product;
-import com.project.musinsastocknotificationbot.product.domain.vo.ProductInfo;
 import com.project.musinsastocknotificationbot.product.domain.repository.ProductRepository;
 import com.project.musinsastocknotificationbot.product.error.JsoupIOException;
+import com.project.musinsastocknotificationbot.product.service.ProductService;
 import com.project.musinsastocknotificationbot.telegramBot.domain.TelegramBotCommand;
 import com.project.musinsastocknotificationbot.telegramBot.infrastructure.TelegramWebClient;
-import com.project.musinsastocknotificationbot.telegramBot.error.CustomTelegramApiException;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -20,16 +19,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 @Service
 public class TelegramService extends TelegramLongPollingBot {
 
     private final String telegramToken;
+    private final ProductService productService;
     private final ProductRepository productRepository;
     private final WebClient webClient;
     private final String chatId;
@@ -38,13 +33,19 @@ public class TelegramService extends TelegramLongPollingBot {
     private static final String BASE_URL = "https://www.musinsa.com/app/goods/";
 
     public TelegramService(@Value("${secret.telegramToken}") String telegramToken,
-        ProductRepository productRepository, TelegramWebClient telegramWebClient,
+        ProductService productService, ProductRepository productRepository, TelegramWebClient telegramWebClient,
         @Value("${secret.chat_id}") String chatId) {
 
         this.telegramToken = telegramToken;
+        this.productService = productService;
         this.productRepository = productRepository;
         this.webClient = telegramWebClient.getWebClient();
         this.chatId = chatId;
+    }
+
+    public static void sendMessage(String message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setText(message);
     }
 
     @Override
@@ -60,70 +61,19 @@ public class TelegramService extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         String[] inputMessage = update.getMessage().getText().split(" ");
-        TelegramBotCommand telegramBotCommand = TelegramBotCommand.valueOf(inputMessage[0]);
-
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chatId);
-
-        switch (telegramBotCommand) {
-            case ADD -> {
-                ProductInfo productInfo = getProductInfo(inputMessage);
-                long productId = productInfo.getId();
-
-                String musinsaProductUrl = BASE_URL + productId;
-
-                try {
-                    doc = Jsoup.connect(musinsaProductUrl).get();
-                } catch (IOException e) {
-                    throw new JsoupIOException(e);
-                }
-
-                String title = doc.select("span.product_title").text();
-                String imageUrl = doc.select("div.product-img").html();
-
-                Product product = Product.of(productInfo, title, imageUrl);
-                productRepository.save(product);
-            }
-            case FIND_ALL -> {
-                StringBuilder stringBuilder = new StringBuilder();
-
-                productRepository.findAll().forEach(product -> {
-                    stringBuilder.append("Id : ");
-                    stringBuilder.append(product.getProductId().getId());
-                    stringBuilder.append(" Size : ");
-                    stringBuilder.append(product.getProductId().getSize());
-                    stringBuilder.append(" Title : ");
-                    stringBuilder.append(product.getTitle());
-                    stringBuilder.append("\n");
-                });
-
-                sendMessage.setText(stringBuilder.toString());
-            }
-            case DELETE -> {
-                ProductInfo productInfo = getProductInfo(inputMessage);
-
-                productRepository.deleteById(productInfo);
-            }
-            default -> sendMessage.setText("""
+        TelegramBotCommand telegramBotCommand = TelegramBotCommand.valueOfLInput(inputMessage[0]);
+        String defaultMessage = """
                     올바른 명령어를 입력해주세요!
                     1. /add {id},{size}
                     2. /findAll
-                    3. /delete {id},{size}""");
+                    3. /delete {id},{size}""";
+
+        switch (telegramBotCommand) {
+            case ADD -> productService.save(inputMessage);
+            case FIND_ALL -> productService.findAll();
+            case DELETE -> productService.delete(inputMessage);
+            default -> sendMessage(defaultMessage);
         }
-
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            throw new CustomTelegramApiException(e);
-        }
-    }
-
-    private static ProductInfo getProductInfo(String[] inputMessage) {
-        String[] inputProductInfo = inputMessage[1].split(",");
-        long productId = Long.parseLong(inputProductInfo[0]);
-        String productSize = inputProductInfo[1];
-
-        return ProductInfo.from(productId, productSize);
     }
 
     @Scheduled(cron = "0/30 * * * * ?")
@@ -146,10 +96,11 @@ public class TelegramService extends TelegramLongPollingBot {
                 if (target.contains(product.getProductId().getSize()) && !target.contains("(품절)")) {
                     stringBuilder.append(title);
                     stringBuilder.append(" : ");
-                    stringBuilder.append("구매가능");
+                    stringBuilder.append(" 구매가능 ");
                     stringBuilder.append("\n");
                     stringBuilder.append("구매 링크 : ");
                     stringBuilder.append(crawlingUrl);
+
                     productRepository.deleteById(product.getProductId());
                 }
             }
@@ -157,6 +108,7 @@ public class TelegramService extends TelegramLongPollingBot {
 
         if (!stringBuilder.isEmpty()) {
             String encodeResult = URLEncoder.encode(stringBuilder.toString(), StandardCharsets.UTF_8);
+
             webClient.get()
                     .uri(uriBuilder -> uriBuilder
                             .queryParam("chat_id", chatId)
