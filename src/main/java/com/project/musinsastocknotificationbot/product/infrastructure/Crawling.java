89fe6664e2e1403ae4441163a3217ed22f203251
cloudwi <cloudwi@naver.com>
2023-06-05 23:37:11 +0900

    refactor : Crawling 기능 분리
    - 크롤링 기능을 분리해서 책임을 분산 시킴

--- src/main/java/com/project/musinsastocknotificationbot/product/infrastructure/Crawling.java
+++
@@ -1,8 +0,0 @@
-package com.project.musinsastocknotificationbot.product.infrastructure;
-
-import com.project.musinsastocknotificationbot.product.domain.dto.Response.CrawlingResponse;
-
-public interface Crawling {
-
-  public CrawlingResponse crawling (String url);
-}
